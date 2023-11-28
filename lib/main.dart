1d9be7f15eee2c1ed4ecaefb1215be35d21c6fa9
sajusun <sajuislam266@gmail.com>
2023-11-29 01:09:18 +0600

    add  geo lation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/helper/geo_location.dart';
 import 'package:weather_app/ui/weather_homepage.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const WeatherHomepage(),
+      home:  LocationPage(),
     );
   }
 }
